fdfca1f3cd29cbf7a0eb408748d5447e2406757a
Abramov21 <104825295+Abramov21@users.noreply.github.com>
2023-04-06 18:49:25 +0300

    fix generes

--- src/components/MovieDetailsInfo/MovieDetailsInfo.jsx
+++ src/components/MovieDetailsInfo/MovieDetailsInfo.jsx
@@ -2,11 +2,7 @@ import { Link, useLocation } from 'react-router-dom';
 import s from './MovieDetailsInfo.module.css';
 
 export const MovieDetailsInfo = movie => {
-  console.log(movie);
-  const GENRES = movie.movie.genres;
-  console.log(GENRES);
-
-  const userScore = Math.floor(movie.movie.vote_average * 10);
+  const userScore = Math.floor(movie?.movie?.vote_average * 10);
   const imgPath = `https://image.tmdb.org/t/p/w500${movie.movie.poster_path}`;
 
   const location = useLocation();
@@ -18,19 +14,19 @@ export const MovieDetailsInfo = movie => {
         Go back
       </Link>
       <div className={s.card}>
-        <img width="300" src={imgPath} alt={movie.movie.title} />
+        <img width="300" src={imgPath} alt={movie?.movie?.title} />
         <div className={s.info}>
           <h2>{movie.movie.title}</h2>
-          <h2>Data release:{movie.movie.release_date}</h2>
+          <h2>Data release:{movie?.movie?.release_date}</h2>
           <p>{`User Score: ${userScore}%`}</p>
           <h3>Overview</h3>
-          <p>{movie.movie.overview}</p>
+          <p>{movie?.movie?.overview}</p>
           <h3>Genres</h3>
-          {/* <p>
-            {movie.movie.genres.length > 0
-              ? movie.movie.genres.map(item => item.name).join(', ')
+          <p>
+            {movie?.movie?.genres?.length > 0
+              ? movie?.movie?.genres?.map(item => item.name).join(', ')
               : 'no info'}
-          </p> */}
+          </p>
         </div>
       </div>
       <div>
