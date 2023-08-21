28045bd0f6ff30063ca39fb9571355ad09caed76
Abramov21 <104825295+Abramov21@users.noreply.github.com>
2023-08-21 16:31:57 +0300

    fixed

--- src/components/ListFilms/ListFilms.jsx
+++ src/components/ListFilms/ListFilms.jsx
@@ -1,19 +1,34 @@
-import { useEffect, useMemo, useState } from 'react';
 import s from './ListFilms.module.css';
+import { useCallback, useEffect, useState } from 'react';
 import { ItemFilms } from './itemFilms/itemFilms';
 
 export const ListFilms = ({ movies, title }) => {
+  const [state, setState] = useState([]);
+
+  const likeMovies = useCallback(
+    () => JSON.parse(localStorage.getItem('likeMovie')) || [],
+    []
+  );
+
+  useEffect(() => {
+    setState(likeMovies);
+    console.log('рендер ліст філм');
+  }, [likeMovies, state]);
+
   return (
     <div className={s.box}>
       {title && <h1 className={s.mainTitle}>Movies Trending</h1>}
       <ul className={s.list}>
         {movies?.map(({ id, title, poster_path, vote_average }) => {
+          const isLike = state.some(movie => movie.id === id);
           return (
             <ItemFilms
+              key={id}
               id={id}
               title={title}
               poster_path={poster_path}
               vote_average={vote_average}
+              isLike={isLike}
             />
           );
         })}
--- src/components/ListFilms/itemFilms/itemFilms.jsx
+++ src/components/ListFilms/itemFilms/itemFilms.jsx
@@ -1,44 +1,29 @@
 import { Link, useLocation } from 'react-router-dom';
-import React, { useState, useMemo, useEffect } from 'react';
+import React, { useState } from 'react';
 import sprite from '../../../image/symbol-defs.svg';
 import clsx from 'clsx';
 import s from '../ListFilms.module.css';
 
 export const ItemFilms = React.memo(
-  ({ id, title, poster_path, vote_average }) => {
-    const [state, setState] = useState([]);
-
-    const likeMovies = JSON.parse(localStorage.getItem('likeMovie')) || [];
-
-    useEffect(() => {
-      setState(likeMovies);
-      console.log('рендер ліст філм');
-    }, [likeMovies]);
-
-    const isLike = state.some(movie => movie.id === id);
-
+  ({ id, title, poster_path, vote_average, isLike }) => {
     const location = useLocation();
+
     const [isActive, setIsActive] = useState(isLike);
 
     const handleLikeButton = (id, title, poster_path, vote_average) => {
-      setIsActive(true);
-
       const likeMovies = JSON.parse(localStorage.getItem('likeMovie')) ?? [];
       const movie = { id, title, poster_path, vote_average };
       let isMovieId = likeMovies.findIndex(({ id }) => id === movie.id);
-      console.log(isMovieId);
 
       if (isMovieId >= 0) {
-        setIsActive(false);
-
         const newMovies = likeMovies.filter(({ id }) => id !== movie.id);
         window.localStorage.setItem('likeMovie', JSON.stringify(newMovies));
-      }
-
-      if (isMovieId === -1) {
-        const newLikeFilm = [...likeMovies, { ...movie, isActive: !isActive }];
+      } else {
+        const newLikeFilm = [...likeMovies, movie];
         window.localStorage.setItem('likeMovie', JSON.stringify(newLikeFilm));
       }
+
+      setIsActive(!isActive);
     };
 
     const imgPath = `https://image.tmdb.org/t/p/w500${poster_path}`;
@@ -50,7 +35,7 @@ export const ItemFilms = React.memo(
         <button
           className={clsx(
             [s.buttonLike],
-            { [s.active]: isActive },
+            { [s.active]: isLike },
             { inactive: !isActive }
           )}
           onClick={() => handleLikeButton(id, title, poster_path, vote_average)}
